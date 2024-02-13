192cba9ffc05b819c47d4b9e9c16b071340e8035
Altycon <altycon1@gmail.com>
2024-02-12 18:07:43 -0800

    changing sw version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 25;
+const version = 26;
 const staticCacheName = `staticCache-${version}`;
 const imageCacheName = `imageCache-${version}`;
 const dynamicCacheName = `dynamicCache`;
