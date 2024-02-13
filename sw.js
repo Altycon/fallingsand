34b5bc615e621008df198b3042e54772041c286d
Altycon <altycon1@gmail.com>
2024-02-12 18:39:44 -0800

    manifest scope path change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 31;
+const version = 32;
 const staticCacheName = `staticCache-${version}`;
 const imageCacheName = `imageCache-${version}`;
 const dynamicCacheName = `dynamicCache`;
